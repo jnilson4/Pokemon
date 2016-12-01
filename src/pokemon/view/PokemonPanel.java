b279fe942c8e10378c3035a4472ad7ccb53da597
Jake Nilson <jnil0572@740s-pl-132.local>
2016-12-01 12:31:37 -0700

    Added the frame and panel to the project.

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,39 @@
 package pokemon.view;
 
-public class PokemonPanel
-{
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import pokemon.controller.PokemonController;
 
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private SpringLayout baseLayout;
+	
+	public PokemonPanel(PokemonController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.GRAY);
+		//this.add();
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
